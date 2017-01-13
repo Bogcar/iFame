44eab01a18b77e9f404123bc84345b8977f040b9
Bogcar <carlo.bogani@samtrevano.ch>
2017-01-13 13:47:28 +0100

    Add files via upload

--- Documents/js/script.js
+++ Documents/js/script.js
@@ -1,15 +1,61 @@
-$(document).ready(function() {
-        var x = $(document).width() * 15 / 100;
-        $("#well").css({"width": x});
-
+function setUp() {
         var frame = document.getElementById('frame');
         frame.style.height = (window.innerHeight - 150) + 'px';
 
-        var h = $(window).height() - 150;
-
-        $("iframe").css({"height" : h});
-});
+        console.log(frame.contentWindow.document.body.scrollHeight + 'px');
+}
 
 function frame(url) {
-        $("iframe").attr({'src' : url});
+        document.getElementById('frame').src = url;
 }
+
+$(document).ready(function(){
+
+$("#ingredients").focus(function() {
+	if(document.getElementById('ingredients').value === ''){ 
+        document.getElementById('ingredients').value +='• ';
+}});
+	
+$("#ingredients").keyup(function(event)
+{
+	var keycode = (event.keyCode ? event.keyCode : event.which);
+	if(keycode == '13'){
+		document.getElementById('ingredients').value +='• ';
+	}
+	else if(keycode == '8' && document.getElementById('ingredients').value !=null){
+		document.getElementById('ingredients').value = '• ';
+	}
+	var txtval = document.getElementById('ingredients').value;
+	if(txtval.substr(txtval.length - 1) == '\n'){
+		document.getElementById('ingredients').value = txtval.substring(0,txtval.length - 1);
+	}
+});
+
+var cnt = 1;
+
+$("#procedure").focus(function() {
+	if(document.getElementById('procedure').value === ''){ 
+        document.getElementById('procedure').value += cnt + '. ';
+		cnt ++;
+}});
+	
+$("#procedure").keyup(function(event)
+{
+	var keycode = (event.keyCode ? event.keyCode : event.which);
+	if(keycode == '13'){
+		document.getElementById('procedure').value += cnt + '. ';
+		cnt++;
+	}
+	else if(keycode == '8' && document.getElementById('procedure').value == 0 || document.getElementById('procedure').value == 1){
+		cnt = 1;
+		document.getElementById('procedure').value = cnt + '. ';
+		cnt++;
+	}
+	var txtval = document.getElementById('procedure').value;
+	if(txtval.substr(txtval.length - 1) == '\n'){
+		document.getElementById('procedure').value = txtval.substring(0,txtval.length - 1);
+	}
+});
+
+});
+
